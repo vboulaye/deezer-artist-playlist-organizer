cd50fc58a36e66f83a99b6776e6a35f05b6932cc
Vincent Boulaye <vincent.boulaye@orange.com>
2023-08-22 00:20:49 +0200

    use PUBLIC_ORIGIN

--- src/lib/DeezerConfig.ts
+++ src/lib/DeezerConfig.ts
@@ -3,4 +3,5 @@ import {env} from "$env/dynamic/public";
 export const DeezerConfig = {
     AUTHORIZATION_URL: env.PUBLIC_DEEZER_AUTHORIZATION_URL || "https://connect.deezer.com/oauth/auth.php",
     TOKEN_URL: env.PUBLIC_DEEZER_TOKEN_URL || "https://connect.deezer.com/oauth/access_token.php",
+    API_URL: env.PUBLIC_DEEZER_API_URL || "https://api.deezer.com",
 }
