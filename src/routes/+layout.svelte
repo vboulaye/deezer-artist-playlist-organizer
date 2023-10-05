973e1f506743d80c5ff50b6dfa6c49d18b3e21ad
Vincent Boulaye <vincent.boulaye@orange.com>
2023-10-05 14:01:16 +0200

    tos+github badge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,6 +42,7 @@
 
 
 </script>
+<a href="https://github.com/vboulaye/deezer-artist-playlist-organizer" class="xtralargeonly absolute right-0"><img decoding="async" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" loading="lazy" data-recalc-dims="1"></a>
 
 <LoadingVeil/>
 <Toast/>
