abdd88e2187d0573096208d9db5bc7109779054a
Vincent Boulaye <vincent.boulaye@orange.com>
2023-10-10 00:08:12 +0200

    trailingslash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,7 @@ import {callDeezer} from "$lib/DeezerCall";
 import type {LayoutLoad} from "./$types"
 
 export const prerender = 'auto'
+export const trailingSlash = 'always';
 
 function getCurrentUser() {
     if (TokenCookie.get()) {
